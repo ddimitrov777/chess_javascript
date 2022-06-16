dbdfc3f746e6a161f6320256cf29cbf1f8b5715e
ddimitrov777 <ddimitro@chicagobooth.edu>
2022-06-16 03:23:20 +0000

    test

---
+++ src/components/chessboard.js
@@ -0,0 +1,30 @@
+//use the stuff withmoveintegration
+//https://codesandbox.io/s/x332zqpkl4?from-embed=&file=/src/integrations/WithMoveValidation.js
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Chessboard from "chessboardjsx";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Chessboard position="start" />
+//         <p>
+//           Chess... <code>code tag</code> 
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Other React Link
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
